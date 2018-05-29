8f4c043b7753676bf0f637d353afab28cce0b39b
Povilas Jurcys <povilas@samesystem.com>
2018-05-29 08:13:17 +0300

    Fix argument type detection by name with bang mark

--- lib/graphql_rails/attribute.rb
+++ lib/graphql_rails/attribute.rb
@@ -9,7 +9,8 @@ module GraphqlRails
     attr_reader :name, :graphql_field_type, :property, :type_name, :description
 
     def initialize(name, type = nil, description: nil, property: name)
-      @name = name.to_s
+      @original_name = name.to_s
+      @name = @original_name.tr('!', '')
       @type_name = type.to_s
       @graphql_field_type = parse_type(type || type_by_attribute_name)
       @description = description
@@ -33,15 +34,20 @@ module GraphqlRails
 
     private
 
+    attr_reader :original_name
+
     def type_by_attribute_name
-      case name
-      when 'id', /_id\Z/
-        GraphQL::ID_TYPE
-      when /\?\Z/
-        GraphQL::BOOLEAN_TYPE
-      else
-        GraphQL::STRING_TYPE
-      end
+      type = \
+        case name
+        when 'id', /_id\Z/
+          GraphQL::ID_TYPE
+        when /\?\Z/
+          GraphQL::BOOLEAN_TYPE
+        else
+          GraphQL::STRING_TYPE
+        end
+
+      original_name['!'] ? type.to_non_null_type : type
     end
 
     def parse_type(type)

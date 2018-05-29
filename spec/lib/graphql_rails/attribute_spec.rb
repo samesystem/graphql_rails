1f5f3647088ad3f7d909a0afd8b3f69daea3a27e
Povilas Jurčys <povilasjurcys@users.noreply.github.com>
2018-05-29 08:15:30 +0300

    Fix argument type detection by name with bang mark (#20)

--- spec/lib/graphql_rails/attribute_spec.rb
+++ spec/lib/graphql_rails/attribute_spec.rb
@@ -12,6 +12,36 @@ module GraphqlRails
     describe '#graphql_field_type' do
       subject(:graphql_field_type) { attribute.graphql_field_type }
 
+      context 'when type is not set' do
+        let(:type) { nil }
+
+        context 'when attribute name ends without bang mark (!)' do
+          it { is_expected.not_to be_non_null }
+        end
+
+        context 'when attribute name ends with bang mark (!)' do
+          let(:name) { :full_name! }
+
+          it { is_expected.to be_non_null }
+        end
+
+        context 'when name ends with question mark (?)' do
+          let(:name) { :admin? }
+
+          it 'returns boolean type' do
+            expect(graphql_field_type).to eq GraphQL::BOOLEAN_TYPE
+          end
+        end
+
+        context 'when name ends with "id"' do
+          let(:name) { :id }
+
+          it 'returns id type' do
+            expect(graphql_field_type).to eq GraphQL::ID_TYPE
+          end
+        end
+      end
+
       context 'when attribute is required' do
         it { is_expected.to be_non_null }
       end

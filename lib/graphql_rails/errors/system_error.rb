f8d9fb2ed3d6fa2b60192024bf7020f0495d11ef
maksim-kulagin-s <maksim.kulagin@samesystem.com>
2022-08-19 12:53:57 +0300

    Update lib/graphql_rails/errors/system_error.rb

--- lib/graphql_rails/errors/system_error.rb
+++ lib/graphql_rails/errors/system_error.rb
@@ -3,13 +3,13 @@
 module GraphqlRails
   # Base class which is returned in case something bad happens. Contains all error rendering structure
   class SystemError < ExecutionError
+    delegate :backtrace, to: :error
+
     def initialize(error)
       super(error.message)
 
       @error = error
     end
-
-    delegate :backtrace, to: :error
 
     def to_h
       super.except('locations')

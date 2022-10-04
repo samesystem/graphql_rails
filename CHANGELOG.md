adb462ca272209508278fda8368f097b2a7ef1ca
Povilas Jurčys <povilasjurcys@users.noreply.github.com>
2022-10-04 13:53:39 +0300

    Skip "base" field in validation errors response (#238)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Added/Changed/Deprecated/Removed/Fixed/Security: YOUR CHANGE HERE
 
 * Added: error backtrace to SystemError
+* Fixed: skip "base" field name in validation error messages
 
 ## [2.2.0](2022-01-25)
 
--- lib/graphql_rails/errors/validation_error.rb
+++ lib/graphql_rails/errors/validation_error.rb
@@ -3,10 +3,12 @@
 module GraphqlRails
   # GraphQL error that is raised when invalid data is given
   class ValidationError < ExecutionError
+    BASE_FIELD_NAME = 'base'
+
     attr_reader :short_message, :field
 
     def initialize(short_message, field)
-      super([field.presence&.to_s&.humanize, short_message].compact.join(' '))
+      super([humanized_field(field), short_message].compact.join(' '))
       @short_message = short_message
       @field = field
     end
@@ -17,6 +19,17 @@ module GraphqlRails
 
     def to_h
       super.merge('field' => field, 'short_message' => short_message)
+    end
+
+    private
+
+    def humanized_field(field)
+      return if field.blank?
+
+      stringified_field = field.to_s
+      return if stringified_field == BASE_FIELD_NAME
+
+      stringified_field.humanize
     end
   end
 end
